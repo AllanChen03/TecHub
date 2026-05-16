f187d03e65bce372c837571741f560d5dc8b9021
AllanChen03 <allan031199@gmail.com>
2026-05-16 03:46:57 -0600

    Configurar email

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -6,9 +6,7 @@ console.log('EMAIL existe:', !!process.env.EMAIL);
 console.log('EMAILPASSWORD existe:', !!process.env.EMAILPASSWORD);
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAILPASSWORD
@@ -17,14 +15,5 @@ const transporter = nodemailer.createTransport({
   greetingTimeout: 10000,
   socketTimeout: 10000
 });
-
-transporter.verify()
-  .then(() => {
-    console.log('Listo para enviar correos de verificación');
-  })
-  .catch((error) => {
-    console.error('Error configurando el correo:', error.message);
-    console.error('Código:', error.code);
-  });
 
 module.exports = transporter;
