a5e4a6c9c74ede76944ce9aafe3447051deeb1ef
AllanChen03 <allan031199@gmail.com>
2026-05-16 03:22:57 -0600

    Configurar archivo server.js

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const usuario = require('./backend/routes/usuario');
-const admin = require('./backend/routes/admin');
-const comentarios = require('./backend/routes/comentarios');
-
+const usuario = require('./routes/usuario');
+const admin = require('./routes/admin');
+const comentarios = require('./routes/comentarios');
 
 const app = express();
 
 app.use(cors({
   origin: '*'
 }));
-app.use(express.json())
+
+app.use(express.json());
 
 app.use(express.static('public'));
 
@@ -23,5 +23,5 @@ app.use('/api/comentarios', comentarios);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
