4a6db234b84b57751f76b605e5c2576115a2c5d6
AllanChen03 <allan031199@gmail.com>
2026-05-16 03:58:31 -0600

    Configurar email

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,6 @@ app.use('/api/comentarios', comentarios);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
