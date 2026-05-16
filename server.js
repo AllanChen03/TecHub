96fa3f4660d7cc4c8dc6e83c93a94385db1db5a2
AllanChen03 <allan031199@gmail.com>
2026-05-16 01:15:55 -0600

    Configurar server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const comentarios = require('./routes/comentarios');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json())
 
 app.use(express.static('public'));
